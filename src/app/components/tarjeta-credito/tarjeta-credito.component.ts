298a93d79c9b21c4ea1b1c0ddb70fc6dccb813b3
fjcampano92 <fjcampano0592@gmail.com>
2022-11-13 19:12:19 -0300

    Validaciones del formulario
    Se agregan validaciones en los campos del formulario agregar tarjeta y en el botón aceptar del mismo formulario.

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -11,9 +12,32 @@ export class TarjetaCreditoComponent implements OnInit {
     { titular:'Fernando Campano', numeroTarjeta:'3423323298', fechaExpiracion: '11/22', cvv:"123"}
   ];
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titular: ['', Validators.required],
+      numeroTarjeta: ['', [Validators.required, Validators.maxLength(16), Validators.minLength(16)]],
+      fechaExpiracion: ['', [Validators.required, Validators.maxLength(5), Validators.minLength(5)]],
+      cvv: ['', [Validators.required, Validators.maxLength(3), Validators.minLength(3)]]
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  agregarTarjeta(){
+    console.log(this.form);  
+    
+    const tarjeta: any = {
+      titular: this.form.get('titular')?.value,
+      numeroTarjeta: this.form.get('numeroTarjeta')?.value,
+      fechaExpiracion: this.form.get('fechaExpiracion')?.value,
+      cvv: this.form.get('cvv')?.value
+    }
+
+    this.listTarjetas.push(tarjeta);
+    this.form.reset;
   }
 
 }
